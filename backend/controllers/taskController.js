dfd463fb5c157bf437dc9f0d0437641d7fb3c653
github_piyush2108 <piyusharmap@gmail.com>
2022-12-27 18:57:43 +0530

    Added APIs for CRUD

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -11,6 +11,7 @@ exports.createTask = async(req, res, next) => {
      })
 }
 
+//get all tasks
 exports.getAllTasks = async(req, res, next) => {
 
      const tasks = await Task.find()
@@ -18,5 +19,49 @@ exports.getAllTasks = async(req, res, next) => {
      res.status(200).json({
           success: true,
           tasks
+     })
+}
+
+//update title, description and category of a task
+exports.updateTask = async(req, res, next) => {
+
+     let task = await Task.findById(req.params.id)
+
+     if(!task){
+          return res.status(500).json({
+               success: false,
+               message: "Task not available"
+          })
+     }
+
+     task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+          new: true,
+          runValidators: true,
+          useFindAndModify: false
+     })
+
+     res.status(200).json({
+          success: true,
+          task
+     })
+}
+
+//remove a task
+exports.removeTask = async(req, res, next) => {
+     
+     const task = await Task.findById(req.params.id)
+
+     if(!task){
+          return res.status(500).json({
+               success: false,
+               message: "Product not available"
+          })
+     }
+
+     await task.remove()
+
+     res.status(200).json({
+          success: true,
+          message: "Task removed successfully"
      })
 }
--- backend/routes/taskRoute.js
+++ backend/routes/taskRoute.js
@@ -1,8 +1,15 @@
 const express = require('express')
-const { getAllTasks, createTask } = require('../controllers/taskController')
+const { getAllTasks, createTask, updateTask, removeTask } = require('../controllers/taskController')
 const router = express.Router()
 
-router.route("/getAllTasks").get(getAllTasks)
+//create new task
 router.route("/create").post(createTask)
+//get all tasks
+router.route("/tasks").get(getAllTasks)
+//update title, desc and category of task
+router.route("/tasks/:id").put(updateTask)
+//remove a task
+router.route("/tasks/:id").delete(removeTask)
+
 
 module.exports = router
